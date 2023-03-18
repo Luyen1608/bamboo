88fe80f637de40a6533f030e20271b51253f868c
Luyen1608 <luyenday1@gmail.com>
2023-03-19 02:16:27 +0700

    luyendv update business detail product

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,3 +17,14 @@ coll[0].addEventListener("click", function() {
     $("#showmore").html("Thu gọn");
     }
 });
+$(".pr-slider").slick({
+    dots: true,
+    prevArrow:
+      '<a class="slick-prev" href="#"><i data-icon="ei-arrow-left" data-size="m"></i></a>',
+    nextArrow:
+      '<a class="slick-next" href="#"><i data-icon="ei-arrow-right" data-size="m"></i></a>',
+    customPaging: function (slick, index) {
+      var targetImage = slick.$slides.eq(index).find("img").attr("src");
+      return '<img src=" ' + targetImage + ' "/>';
+    },
+  });
